0f04c5fc35d9464c01fa4847f4e03cab633289b5
Folusho <folusho@lanthanidesoft.com>
2016-05-17 15:57:01 +0100

    implemented the method for adding new notes to our list of notes

--- noteapplication.js
+++ noteapplication.js
@@ -1,4 +1,8 @@
 function NotesApplication(author) {
   this.author = author;
-  this.list_of_notes = [];
-}
+  this.list_of_notes = []; 
+};
+
+NotesApplication.prototype.create = function (notes_to_add) {
+  this.list_of_notes.append(notes_to_add);
+};
