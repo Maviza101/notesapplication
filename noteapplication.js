01b4868e9e97f6843d1ebddd56766a3d931bf18f
Folusho <folusho@lanthanidesoft.com>
2016-05-17 16:14:06 +0100

    implemented the method for listing previously saved notes

--- noteapplication.js
+++ noteapplication.js
@@ -6,3 +6,16 @@ function NotesApplication(author) {
 NotesApplication.prototype.create = function (notes_to_add) {
   this.list_of_notes.append(notes_to_add);
 };
+
+NotesApplication.prototype.listNotes = function () {
+  if(this.list_of_notes == []) {return "There are no notes in this list."}
+  else {
+    for(i=0; i < this.list_of_notes.length; i++) {
+      console.log("Note ID: "+i+"\n"+\
+                  this.list_of_notes[i]+"\n\n"+\
+                  "By Author "+this.author
+                  
+                 );
+    }
+  }
+}
